e6d7a62b085b0fe1210ede292edaad72b85ec902
Philip Guo <philip@pgbovine.net>
2015-10-04 18:02:29 +0000

    bam

--- vg_to_opt_trace.py
+++ vg_to_opt_trace.py
@@ -3,8 +3,15 @@
 
 # Created 2015-10-04 by Philip Guo
 
+# pass in the $basename of a program. assumes that the trace is
+# $basename.trace and the source file is $basename.c
+
+
 import json
+import pprint
 import sys
+
+pp = pprint.PrettyPrinter(indent=2)
 
 RECORD_SEP = '=== pg_trace_inst ==='
 
@@ -23,13 +30,45 @@ def process_record(lines):
 
 
 def process_json_obj(obj):
-    #print obj
-    pass
+    print '---'
+    #pp.pprint(obj)
+    #print
+
+    assert len(obj['stack']) > 0 # C programs always have a main at least!
+    obj['stack'].reverse() # make the stack grow down to follow convention
+
+
+    # create an execution point object
+    ret = {}
+
+    heap = {}
+    stack = []
+    globals_obj = {}
+
+    ret['ordered_globals'] = obj['ordered_globals']
+
+    ret['line'] = obj['line']
+    ret['func_name'] = obj['stack'][-1]['func_name'] # use the 'topmost' (last) entry
+
+    # TODO: handle more event types
+    ret['event'] = 'step_line'
+    ret['stdout'] = '' # TODO: handle this
+
+    ret['heap'] = heap
+    ret['stack_to_render'] = stack
+    ret['globals'] = globals_obj
+
+    pp.pprint(ret)
+    print
+
+    return ret
 
 
 if __name__ == '__main__':
+    basename = sys.argv[1]
+    cod = open(basename + '.c').read()
     cur_record_lines = []
-    for line in open(sys.argv[1]):
+    for line in open(basename + '.trace'):
         line = line.strip()
         if line == RECORD_SEP:
             process_record(cur_record_lines)
