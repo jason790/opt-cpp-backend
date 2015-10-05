36a8e528ca5a6db58a79411b012f2c5cc421d908
Philip Guo <philip@pgbovine.net>
2015-10-05 03:43:55 +0000

    bah

--- vg_to_opt_trace.py
+++ vg_to_opt_trace.py
@@ -7,6 +7,26 @@
 # $basename.trace and the source file is $basename.c
 
 
+# this is pretty brittle and dependent on the user's gcc version and
+# such because it generates code to conform to certain calling
+# conventions, frame pointer settings, etc., eeek
+#
+# we're assuming that the user has compiled with:
+# gcc -ggdb -O0 -fno-omit-frame-pointer
+#
+# on a platform like:
+'''
+$ gcc -v
+Using built-in specs.
+COLLECT_GCC=gcc
+COLLECT_LTO_WRAPPER=/usr/lib/gcc/x86_64-linux-gnu/4.8/lto-wrapper
+Target: x86_64-linux-gnu
+Configured with: ../src/configure -v --with-pkgversion='Ubuntu 4.8.4-2ubuntu1~14.04' --with-bugurl=file:///usr/share/doc/gcc-4.8/README.Bugs --enable-languages=c,c++,java,go,d,fortran,objc,obj-c++ --prefix=/usr --program-suffix=-4.8 --enable-shared --enable-linker-build-id --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --with-gxx-include-dir=/usr/include/c++/4.8 --libdir=/usr/lib --enable-nls --with-sysroot=/ --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --enable-gnu-unique-object --disable-libmudflap --enable-plugin --with-system-zlib --disable-browser-plugin --enable-java-awt=gtk --enable-gtk-cairo --with-java-home=/usr/lib/jvm/java-1.5.0-gcj-4.8-amd64/jre --enable-java-home --with-jvm-root-dir=/usr/lib/jvm/java-1.5.0-gcj-4.8-amd64 --with-jvm-jar-dir=/usr/lib/jvm-exports/java-1.5.0-gcj-4.8-amd64 --with-arch-directory=amd64 --with-ecj-jar=/usr/share/java/eclipse-ecj.jar --enable-objc-gc --enable-multiarch --disable-werror --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --with-tune=generic --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu
+Thread model: posix
+gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04)
+'''
+
+
 import json
 import pprint
 import sys
@@ -14,6 +34,8 @@ import sys
 pp = pprint.PrettyPrinter(indent=2)
 
 RECORD_SEP = '=== pg_trace_inst ==='
+
+all_execution_points = []
 
 def process_record(lines):
     if not lines:
@@ -23,14 +45,14 @@ def process_record(lines):
     #print rec
     try:
         obj = json.loads(rec)
-        process_json_obj(obj)
+        x = process_json_obj(obj)
+        all_execution_points.append(x)
     except ValueError:
-        print >> sys.stderr, "Ugh, died!"
-        sys.exit(1) # we dead!
+        print >> sys.stderr, "Ugh, bad record!"
 
 
 def process_json_obj(obj):
-    print '---'
+    #print '---'
     #pp.pprint(obj)
     #print
 
@@ -53,7 +75,6 @@ def process_json_obj(obj):
     ret['line'] = obj['line']
     ret['func_name'] = top_stack_entry['func_name'] # use the 'topmost' entry's name
 
-    # TODO: handle more event types
     ret['event'] = 'step_line'
     ret['stdout'] = '' # TODO: handle this
 
@@ -86,8 +107,8 @@ def process_json_obj(obj):
             enc_locals[local_var] = encode_value(local_val, heap)
 
 
-    pp.pprint(ret)
-    print
+    #pp.pprint(ret)
+    #print [(e['func_name'], e['frame_id']) for e in ret['stack_to_render']]
 
     return ret
 
@@ -139,7 +160,6 @@ def encode_value(obj, heap):
 
 if __name__ == '__main__':
     basename = sys.argv[1]
-    cod = open(basename + '.c').read()
     cur_record_lines = []
     for line in open(basename + '.trace'):
         line = line.strip()
@@ -150,3 +170,86 @@ if __name__ == '__main__':
             cur_record_lines.append(line)
 
     process_record(cur_record_lines) # process final record
+
+    print len(all_execution_points)
+
+    # now do some filtering action based on heuristics
+    filtered_execution_points = []
+
+    for pt in all_execution_points:
+        # any execution point with a 0x0 frame pointer is bogus
+        frame_ids = [e['frame_id'] for e in pt['stack_to_render']]
+        func_names = [e['func_name'] for e in pt['stack_to_render']]
+        if '0x0' in frame_ids:
+            continue
+
+        # any point with DUPLICATE frame_ids is bogus, since it means
+        # that the frame_id of some frame hasn't yet been updated
+        if len(set(frame_ids)) < len(frame_ids):
+            continue
+
+        # any point with a weird '???' function name is bogus
+        # but we shouldn't have any more by now
+        assert '???' not in func_names
+
+        #print func_names, frame_ids
+        filtered_execution_points.append(pt)
+
+
+    final_execution_points = []
+    if filtered_execution_points:
+        final_execution_points.append(filtered_execution_points[0])
+        # finally, make sure that each successive entry contains
+        # frame_ids that are either identical to the previous one, or
+        # differ by the addition or subtraction of one element at the
+        # end, which represents a function call or return, respectively.
+        # there are weird cases like:
+        #
+        # [u'main'] [u'0xFFEFFFE30']
+        # [u'main'] [u'0xFFEFFFE30']
+        # [u'foo'] [u'0xFFEFFFDC0'] <- bogus
+        # [u'main', u'foo'] [u'0xFFEFFFE30', u'0xFFEFFFDC0']
+        # [u'main', u'foo'] [u'0xFFEFFFE30', u'0xFFEFFFDC0']
+        #
+        # where the middle entry should be FILTERED OUT since it's
+        # missing 'main' for some reason
+        for prev, cur in zip(filtered_execution_points, filtered_execution_points[1:]):
+            prev_frame_ids = [e['frame_id'] for e in prev['stack_to_render']]
+            cur_frame_ids = [e['frame_id'] for e in cur['stack_to_render']]
+
+            # identical, we're good to go
+            if prev_frame_ids == cur_frame_ids:
+                final_execution_points.append(cur)
+            elif len(prev_frame_ids) < len(cur_frame_ids):
+                # cur_frame_ids is prev_frame_ids + 1 extra element on
+                # the end -> function call
+                if prev_frame_ids == cur_frame_ids[:-1]:
+                    final_execution_points.append(cur)
+            elif len(prev_frame_ids) > len(cur_frame_ids):
+                # cur_frame_ids is prev_frame_ids MINUS the last element on
+                # the end -> function return
+                if cur_frame_ids == prev_frame_ids[:-1]:
+                    final_execution_points.append(cur)
+
+        assert len(final_execution_points) <= len(filtered_execution_points)
+
+        # now mark 'call' and' 'return' events via the same heuristic as above
+        for prev, cur in zip(final_execution_points, final_execution_points[1:]):
+            prev_frame_ids = [e['frame_id'] for e in prev['stack_to_render']]
+            cur_frame_ids = [e['frame_id'] for e in cur['stack_to_render']]
+
+            if len(prev_frame_ids) < len(cur_frame_ids):
+                if prev_frame_ids == cur_frame_ids[:-1]:
+                    cur['event'] = 'call'
+            elif len(prev_frame_ids) > len(cur_frame_ids):
+                if cur_frame_ids == prev_frame_ids[:-1]:
+                    prev['event'] = 'return'
+        # make the final entry a 'return' (presumably from main) just for posterity
+        final_execution_points[-1]['event'] = 'return'
+
+
+    for elt in final_execution_points:
+        print elt['event'], [e['func_name'] for e in elt['stack_to_render']]
+
+    cod = open(basename + '.c').read()
+
