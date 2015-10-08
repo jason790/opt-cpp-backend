1af09276d03dd199bacd515c59b5b676672eb01d
Philip Guo <philip@pgbovine.net>
2015-10-08 07:10:49 +0000

    lalala

--- tests/run_test_from_scratch.py
+++ tests/run_test_from_scratch.py
@@ -8,7 +8,10 @@ if os.path.exists(trace_f):
     os.remove(trace_f)
 
 subprocess.call(["make", trace_f]) # synchronous
+assert os.path.exists(trace_f)
 
 # print out trace_f to stdout
 for line in open(trace_f):
     print line,
+
+os.remove(trace_f) # clean up
